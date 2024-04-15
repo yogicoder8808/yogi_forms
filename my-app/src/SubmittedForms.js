be00af612bc8c6c93b2014eb8931a62de2513a26
yogicoder8808 <yogeshwaran383@gmail.com>
2024-04-15 17:08:38 +0530

    Code modified for sumbitted forms

--- my-app/src/SubmittedForms.js
+++ my-app/src/SubmittedForms.js
@@ -1,34 +1,41 @@
-import React from "react";
-import { useFormContext } from "./FormContext";
+import React, { useEffect, useState } from "react";
+
 
 function SubmittedFormPage () {
-   const {submittedForms} = useFormContext()
+    const [formDataList, setFormDataList] = useState([])
 
+    useEffect (() => {
+        fetchFormDta();
+    },[]);
 
-return(
-    <div className='form-group1'>
-        <h2>Submitted Data</h2>
-        {submittedForms.map ((form, index) => (
-            <div key={index}> 
-                <p>Name: {form.name}</p>
-                <p>Email: {form.email}</p>
-                <p>Message: {form.message}</p>
-                {form.attachment && (
-                    <div>
-                        <p>Attachment:</p>
-                        {typeof form.attachment === 'string' ? (
-                            <img src = {URL.createObjectURL (form.attachment)} alt = "Attachment" style={ { maxWidth: '100px', maxHeight: '100px'}}/> 
-                            ) : (
-                            <a href = {URL.createObjectURL (form.attachment)} download> Download Attachment</a>
-                            )}
-                    </div> 
-                )}
-                        
-            </div>
-        ))}
-    </div>
-)
+    const fetchFormDta = async () => {
+        try{
+            const response = await fetch ('http://localhost:4000/forms')
+        if (!response.ok) {
+            throw new Error ('Failedto fetch form data')
+        }
+        const data = await response.json();
+        setFormDataList(data);
+    } catch (error) {
+        console.error('Error fetching form data:', error)
+    }
+}
 
+    return(
+        <div>
+            <h2>Submitted Form</h2>
+            <ul>
+                {formDataList.map ((formData, index) => (
+                    <li key={index}>
+                        <h3>Form {index + 1}</h3>
+                        <p>Name: {formData.name}</p>
+                        <p>Email: {formData.email}</p>
+                        <p>Message: {formData.message}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default SubmittedFormPage;
@@ -63,3 +70,33 @@ export default SubmittedFormPage;
 // )
 
 // }
+
+
+
+
+// const {submittedForms} = useFormContext()
+
+
+// return(
+//     <div className='form-group1'>
+//         <h2>Submitted Data</h2>
+//         {submittedForms.map ((form, index) => (
+//             <div key={index}> 
+//                 <p>Name: {form.name}</p>
+//                 <p>Email: {form.email}</p>
+//                 <p>Message: {form.message}</p>
+//                 {form.attachment && (
+//                     <div>
+//                         <p>Attachment:</p>
+//                         {typeof form.attachment === 'string' ? (
+//                             <img src = {URL.createObjectURL (form.attachment)} alt = "Attachment" style={ { maxWidth: '100px', maxHeight: '100px'}}/> 
+//                             ) : (
+//                             <a href = {URL.createObjectURL (form.attachment)} download> Download Attachment</a>
+//                             )}
+//                     </div> 
+//                 )}
+                        
+//             </div>
+//         ))}
+//     </div>
+// )
